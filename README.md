2718a159d5950591a0faa4b092f13e2e495e2569
yizhi.li <tgxpuisb@hotmail.com>
2017-03-08 00:34:04 +0800

    自定义形状学习

--- README.md
+++ README.md
@@ -10,3 +10,8 @@
 2. [第1章--了解three.js](https://github.com/tgxpuisb/learning/blob/master/webgl/1.md)
 3. [第2章--了解照相机](https://github.com/tgxpuisb/learning/blob/master/webgl/2.md)
 4. [第3章--基本形状学习](https://github.com/tgxpuisb/learning/blob/master/webgl/3.md)
+
+
+### 外链(外部链接,优秀资料引用,供学习使用)
+
+1. [那些年坑过我们的浏览器滚动事件](http://w3help.org/zh-cn/causes/SD9013)
--- webgl/demo/4.js
+++ webgl/demo/4.js
@@ -158,7 +158,7 @@ scene.add(Torus)
 */
 
 // 圆环节
-
+/*
 var Torus = new THREE.Mesh(
 	new THREE.TorusKnotGeometry(2, 0.5, 32, 8),
 	new THREE.MeshBasicMaterial({
@@ -167,6 +167,50 @@ var Torus = new THREE.Mesh(
 	})
 )
 scene.add(Torus)
+*/
+
+//自定义形状
+// 初始化几何形状
+var geometry = new THREE.Geometry();
+
+// 设置顶点位置
+// 顶部4顶点
+geometry.vertices.push(new THREE.Vector3(-1, 2, -1));
+geometry.vertices.push(new THREE.Vector3(1, 2, -1));
+geometry.vertices.push(new THREE.Vector3(1, 2, 1));
+geometry.vertices.push(new THREE.Vector3(-1, 2, 1));
+// 底部4顶点
+geometry.vertices.push(new THREE.Vector3(-2, 0, -2));
+geometry.vertices.push(new THREE.Vector3(2, 0, -2));
+geometry.vertices.push(new THREE.Vector3(2, 0, 2));
+geometry.vertices.push(new THREE.Vector3(-2, 0, 2));
+
+// 设置顶点连接情况
+// 顶面
+geometry.faces.push(new THREE.Face3(0, 1, 3));
+geometry.faces.push(new THREE.Face3(1, 2, 3));
+// 底面
+geometry.faces.push(new THREE.Face3(4, 5, 6));
+geometry.faces.push(new THREE.Face3(5, 6, 7));
+// 四个侧面
+geometry.faces.push(new THREE.Face3(1, 5, 6));
+geometry.faces.push(new THREE.Face3(6, 2, 1));
+geometry.faces.push(new THREE.Face3(2, 6, 7));
+geometry.faces.push(new THREE.Face3(7, 3, 2));
+geometry.faces.push(new THREE.Face3(3, 7, 0));
+geometry.faces.push(new THREE.Face3(7, 4, 0));
+geometry.faces.push(new THREE.Face3(0, 4, 5));
+geometry.faces.push(new THREE.Face3(0, 5, 1));
+
+var customGeometry = new THREE.Mesh(
+	geometry,
+	new THREE.MeshBasicMaterial({
+		color: 0xff4956,
+		wireframe: true
+	})
+)
+
+scene.add(customGeometry)
 
 
 
