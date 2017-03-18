9120f4a4ca8c9d22d6ac24ddb6d35a206dc93316
yizhi.li <tgxpuisb@hotmail.com>
2017-03-19 00:50:45 +0800

    光与影

--- webgl/demo/8.js
+++ webgl/demo/8.js
@@ -44,12 +44,56 @@ var light = new THREE.AmbientLight(0xff0000)
 scene.add(light)
 */
 
+/*
 // 点光源
-
 var light = new THREE.PointLight(0xffffff, 2, 100)
 light.position.set(0, 1.5, 3)
 scene.add(light)
+*/
 
+/*
+//平行光
+var light = new THREE.DirectionalLight(0xffffff)
+light.position.set(2, 5, 3)
+scene.add(light)
+*/
+
+var xgeometry = new THREE.Geometry()
+xgeometry.vertices.push(new THREE.Vector3(0, 0, 0))
+xgeometry.vertices.push(new THREE.Vector3(20, 0, 0))
+var xline = new THREE.Line(
+	xgeometry,
+	new THREE.LineBasicMaterial({
+		color: 0xff0000
+	})
+)
+var ygeometry = new THREE.Geometry()
+ygeometry.vertices.push(new THREE.Vector3(0, 0, 0))
+ygeometry.vertices.push(new THREE.Vector3(0, 20, 0))
+var yline = new THREE.Line(
+	ygeometry,
+	new THREE.LineBasicMaterial({
+		color: 0x00ff00
+	})
+)
+var zgeometry = new THREE.Geometry()
+zgeometry.vertices.push(new THREE.Vector3(0, 0, 0))
+zgeometry.vertices.push(new THREE.Vector3(0, 0, 20))
+var zline = new THREE.Line(
+	zgeometry,
+	new THREE.LineBasicMaterial({
+		color: 0x0000ff
+	})
+)
+scene.add(xline, yline, zline)
+
+
+
+//聚光灯
+var light = new THREE.SpotLight(0xffffff, 1, 100, Math.PI / 6)
+light.position.set(3, 3, 3)
+light.target.position.set(0, 0, 0)
+scene.add(light)
 
 renderer.render(scene, camera)
 
