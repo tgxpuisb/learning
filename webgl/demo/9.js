bb2b1afac7bc913f6a42f72de33f75185b75b1e2
yizhi.li <tgxpuisb@hotmail.com>
2017-03-21 23:52:00 +0800

    网格

--- webgl/demo/9.js
+++ webgl/demo/9.js
@@ -21,37 +21,37 @@ camera.position.set(3, 3, 3)
 scene.add(camera)
 camera.lookAt(new THREE.Vector3(0, 0, 0)) //照相机看坐标轴的方向
 
-renderer.shadowMapEnabled = true;
-renderer.shadowMapSoft = true;
+renderer.shadowMapEnabled = true
+renderer.shadowMapSoft = true
 
 var plane = new THREE.Mesh(new THREE.PlaneGeometry(8, 8, 16, 16),
-        new THREE.MeshLambertMaterial({color: 0xcccccc}));
+        new THREE.MeshLambertMaterial({color: 0xcccccc}))
 plane.rotation.x = -Math.PI / 2;
-plane.position.y = -1;
-plane.receiveShadow = true;
-scene.add(plane);
+plane.position.y = -1
+plane.receiveShadow = true
+scene.add(plane)
 
 cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1),
-        new THREE.MeshLambertMaterial({color: 0x00ff00}));
-cube.position.x = 2;
-cube.castShadow = true;
-scene.add(cube);
+        new THREE.MeshLambertMaterial({color: 0x00ff00}))
+cube.position.x = 2
+cube.castShadow = true
+scene.add(cube)
 
-var light = new THREE.SpotLight(0xffff00, 1, 100, Math.PI / 6, 25);
-light.position.set(2, 5, 3);
-light.target = cube;
-light.castShadow = true;
+var light = new THREE.SpotLight(0xffff00, 1, 100, Math.PI / 6, 25)
+light.position.set(2, 5, 3)
+light.target = cube
+light.castShadow = true
 
-light.shadowCameraNear = 2;
-light.shadowCameraFar = 10;
-light.shadowCameraFov = 30;
-light.shadowCameraVisible = true;
+light.shadowCameraNear = 2
+light.shadowCameraFar = 10
+light.shadowCameraFov = 30
+light.shadowCameraVisible = true
 
-light.shadowMapWidth = 1024;
-light.shadowMapHeight = 1024;
-light.shadowDarkness = 0.3;
+light.shadowMapWidth = 1024
+light.shadowMapHeight = 1024
+light.shadowDarkness = 0.3
 
-scene.add(light);
+scene.add(light)
 
 renderer.render(scene, camera)
 
